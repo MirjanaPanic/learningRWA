29e20020d63bc0b8683571969ecb8c8bcdde29ca
MirjanaPanic <panicmirjanaa@gmail.com>
2025-07-01 20:20:35 +0200

    combineLatest

--- src/combinationOperators.ts
+++ src/combinationOperators.ts
@@ -1,23 +1,31 @@
-// withLatestFrom
-//npr pozicija misa da bude u toku iz kojeg se uzima poslednja vrednost
-//a da osnovni tok bude pritisak nekog tastera
-//da pritisak tastera potvrdi poslednju vrednost iz toka pozicije misa(mozda cak klik)
+import { combineLatest, map, Observable, Subject, zip } from "rxjs";
 
-import { map, Observable, Subject, zip } from "rxjs";
-
-// zip
 const flowNumbers$ = new Subject<number>();
 const flowStrings$ = new Subject<string>();
+
+// zip, combineLatest
 
 const zipFlow$: Observable<[number, string]> = zip([
   flowNumbers$,
   flowStrings$,
 ]);
-zipFlow$.subscribe((tuple) => console.log(tuple));
+zipFlow$.subscribe((tuple) => console.log("zip ", tuple));
+//flowNumbers$.subscribe((x) => console.log("numbers", x));
 
-flowNumbers$.next(1);
-flowNumbers$.next(2);
-flowNumbers$.next(3);
+const combineLatestFlow$: Observable<[number, string]> = combineLatest([
+  flowNumbers$,
+  flowStrings$,
+]);
+combineLatestFlow$.subscribe((tuple) => console.log("combineLatest ", tuple));
+
 flowStrings$.next("jedan");
 flowStrings$.next("dva");
 flowStrings$.next("tri");
+flowNumbers$.next(1);
+flowNumbers$.next(2);
+flowNumbers$.next(3);
+
+// withLatestFrom
+//npr pozicija misa da bude u toku iz kojeg se uzima poslednja vrednost
+//a da osnovni tok bude pritisak nekog tastera
+//da pritisak tastera potvrdi poslednju vrednost iz toka pozicije misa(mozda cak klik)
